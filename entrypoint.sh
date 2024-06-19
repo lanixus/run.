c6ea5f6d19b772b6ef1a87727bd979c052072513
lanixus <154478794+lanixus@users.noreply.github.com>
2024-06-19 05:19:03 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/container
+
+# Dar permisos de ejecución al script principal (start.sh en este caso)
+chmod +x start.sh
+
+# Ejecutar el script principal (start.sh)
+./start.sh
